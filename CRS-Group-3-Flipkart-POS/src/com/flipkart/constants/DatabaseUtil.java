01f7354dcc2b74a619ab461e631560828788ce02
shreyansh0212 <shreyansh.soni@flipkart.com>
2022-06-25 05:06:16 +0530

    databaseutil

--- CRS-Group-3-Flipkart-POS/src/com/flipkart/constants/DatabaseUtil.java
+++ CRS-Group-3-Flipkart-POS/src/com/flipkart/constants/DatabaseUtil.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseUtil {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost/CRSDatabase";
 
     //  Database credentials
@@ -14,7 +14,7 @@ public class DatabaseUtil {
 
     public static Connection getConn() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(JDBC_DRIVER);
             // Step 4 make/open  a connection
 
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
